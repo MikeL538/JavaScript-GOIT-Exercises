e86ee24fdd1afab8df0270f1128e44ee070be26a
MikeL538 <126614886+MikeL538@users.noreply.github.com>
2023-08-05 14:46:02 +0200

    Update
    Infinite scroll added

--- src/tasks/6_images_browser/image_browser.js
+++ src/tasks/6_images_browser/image_browser.js
@@ -1,6 +1,6 @@
 import Notiflix from 'notiflix';
-
 import SimpleLightbox from 'simplelightbox';
+import InfiniteScroll from 'infinite-scroll';
 
 const searchButton = document.querySelector(
   '.images_browser__search-form__button'
@@ -8,45 +8,39 @@ const searchButton = document.querySelector(
 const searchInput = document.querySelector(
   '.images_browser__search-form__input'
 );
-const loadMoreButton = document.querySelector(
-  '.images_browser__load-more__button'
-);
 const galleryList = document.querySelector('.images_browser__gallery__list');
+
 const pixabayKey = '38531038-07b18ea2bd70e8e8bef0f3931';
 
 let page = 1;
 let totalPages = 0;
-
-loadMoreButton.style.display = 'none';
+let isLoading = false;
 
 searchButton.addEventListener('click', async event => {
   event.preventDefault();
   galleryList.innerHTML = '';
-  loadMoreButton.style.display = 'none';
+  page = 1;
+  isLoading = true; // Set isLoading to true when search button is clicked
 
   try {
     await getImages();
   } catch (error) {
     console.error('Error while fetching images:', error);
   }
+
+  isLoading = false; // Set isLoading to false after images are loaded
 });
 
 async function getImages() {
-  page = 1;
   try {
     const data = await fetchImages(page);
     if (data.hits.length === 0) {
-      loadMoreButton.style.display = 'none';
       Notiflix.Notify.failure(
         'Sorry, there are no images matching your search query. Please try again.'
       );
     } else {
       totalPages = data.totalHits / 20;
-      if (data.totalHits > 20) {
-        loadMoreButton.style.display = 'block';
-      }
       renderImages(data);
-
       const totalHits = data.totalHits;
       Notiflix.Notify.info(`Hooray! We found ${totalHits} images.`);
     }
@@ -69,13 +63,10 @@ async function fetchImages(page) {
 }
 
 async function renderImages(data) {
-  const markup = data.hits.map(
-    ({ id, webformatURL, likes, views, comments, downloads, tags }, index) => {
-      // ID for smooth scrolling
-      const idAttribute = index === 0 ? `id="first-image-${page}"` : '';
-
-      return `
-        <li ${idAttribute}>
+  const markup = data.hits
+    .map(
+      ({ id, webformatURL, likes, views, comments, downloads, tags }) => `
+        <li class="images_browser__gallery__list-item">
           <a href="${webformatURL}"><img src="${webformatURL}" alt="${tags}" loading="lazy" /></a>
           <div class="info">
             <p class="info-item"><b>Likes</b> ${likes}</p>
@@ -84,34 +75,33 @@ async function renderImages(data) {
             <p class="info-item"><b>Downloads</b> ${downloads}</p>
           </div>
         </li>
-      `;
-    }
-  );
-  galleryList.innerHTML += markup.join('');
+      `
+    )
+    .join('');
 
-  const lightBox = new SimpleLightbox('.images_browser__gallery a', {
+  galleryList.innerHTML += markup;
+  lightBoxRender();
+}
+// Loading simplelightbox for new images
+function lightBoxRender() {
+  const lightBox = new SimpleLightbox('.images_browser__gallery__list-item a', {
     captionsData: 'alt',
     captionDelay: 250,
   });
-
-  // Smooth scrolling
-  const firstImage = document.getElementById(`first-image-${page}`);
-  if (firstImage) {
-    firstImage.scrollIntoView({ behavior: 'smooth' });
-  }
 }
 
-loadMoreButton.addEventListener('click', async () => {
-  try {
-    await loadMoreImages();
-  } catch (error) {
-    console.error('Error while fetching more images:', error);
-  }
-});
+// Check if the user has reached the bottom of the page
+function isAtBottom() {
+  return (
+    window.innerHeight + window.scrollY >= document.body.offsetHeight - 150
+  );
+}
 
+// Load more images when the user reaches the bottom
 async function loadMoreImages() {
-  page++;
-  if (page <= totalPages) {
+  if (!isLoading && page < totalPages) {
+    page++;
+    isLoading = true; // Set isLoading to true when loading more images
     try {
       const data = await fetchImages(page);
       renderImages(data);
@@ -119,10 +109,13 @@ async function loadMoreImages() {
       console.error('Error while fetching more images:', error);
       Notiflix.Notify.failure('Error occurred while fetching more images.');
     }
-  } else {
-    loadMoreButton.style.display = 'none';
-    Notiflix.Notify.info(
-      "We're sorry, but you've reached the end of search results."
-    );
+    isLoading = false; // Set isLoading to false after loading more images
   }
 }
+
+// Event listener to trigger loading more images when the user scrolls to the bottom
+window.addEventListener('scroll', () => {
+  if (isAtBottom()) {
+    loadMoreImages();
+  }
+});
